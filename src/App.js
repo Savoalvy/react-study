41f75f57d6d867d927c2d9ad717d2f3c8b07d024
Aliaksandr Savastsianchyk <sava@epolsoft.com>
2024-01-30 11:46:35 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-study">
+      <header className ="App-header">
+      <a
+        className ="App-logo"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXQw3GCT64XQQ5WL2WOcGF0LgRjwzH52GgWDEfDnC-Wg&s"
+          alt="Картинка"
+        ></img>
+      </a>
+    </header>
+    <nav className = "app-list-container">
+        <ul className = "app-list">
+            <li>text text</li>
+            <li>text text</li>
+            <li>text text</li>
+            <li>text text</li>
+            <li>text text</li>
+        </ul>
+    </nav>
     </div>
   );
 }
